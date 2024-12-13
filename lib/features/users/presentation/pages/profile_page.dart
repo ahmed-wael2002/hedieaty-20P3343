8ebba88083f45ccaf982405e9eb0116e30d87502
Ahmed Wael <87478304+ahmed-wael2002@users.noreply.github.com>
2024-12-13 19:02:26 +0200

    Refactoring commit -- removed unnecessary directories and files

--- lib/features/users/presentation/pages/profile_page.dart
+++ lib/features/users/presentation/pages/profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:lecture_code/Pages/UpdateProfilePage.dart';
+import 'package:lecture_code/features/users/presentation/pages/UpdateProfilePage.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
-import '../features/users/domain/entity/user.dart';
+import '../../domain/entity/user.dart';
 
 class ProfilePage extends StatelessWidget {
   final UserEntity user;
